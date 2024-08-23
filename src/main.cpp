d2986072533a08f4dc09cf0ee27b45435c49c1b4
malevitzch <malevitzch@gmail.com>
2024-08-23 12:25:18 +0200

    LLVM test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/raw_ostream.h>
 
 int main()
 {
