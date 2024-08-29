5a7484e47ef61ca39aa5d59e4f94c76894464ba6
malevitzch <malevitzch@gmail.com>
2024-08-29 14:25:07 +0200

    Implemented early testing of lexer, seems good so far

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_ostream.h>
 
+#include "lexer.hpp"
+
 int main()
 {
-  std::cout<< "Hello LLVM!\n";
+
+  std::cout << "Input the name of the file that you want to tokenize: ";
+  std::string filename;
+  std::cin>>filename;
+  auto tokens = get_tokens_from_file(filename);
+  for(auto token : tokens) std::cout << "[" << token.contents << "] ";
 }
