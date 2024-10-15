175cd5f6bbc7a73c329abcbad8f822fcea8f0c08
malevitzch <malevitzch@gmail.com>
2024-10-15 12:28:53 +0200

    Made ASTNode and Expression abstract classes, added simple constructors for BinaryOperator and UnaryOperator

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,11 +1,12 @@
 #include <memory>
+#include <string>
 namespace ast
 {
   class ASTNode
   {
   private:
   public:
-    virtual void codegen();
+    virtual void codegen() = 0;
   };
 
   class Statement : public ASTNode
@@ -18,6 +19,7 @@ namespace ast
   {
   private:
   public:
+    virtual void eval() = 0;
   };
 
   class BinaryOperator : public Expression
@@ -25,14 +27,18 @@ namespace ast
   private:
     std::unique_ptr<Expression> lhs;
     std::unique_ptr<Expression> rhs;
+    std::string name;
   public:
+    BinaryOperator(std::unique_ptr<Expression> lhs, std::unique_ptr<Expression> rhs, std::string name); //TODO: 3rd argument is an object that represents the operator itself.
   };
 
   class UnaryOperator : public Expression
   {
   private:
     std::unique_ptr<Expression> operand;
+    std::string name;
   public:
+    UnaryOperator(std::unique_ptr<Expression> operand, std::string name); //TODO: 2nd argument is an object that represents the operator itself.
   };
   
   class Value : public Expression
